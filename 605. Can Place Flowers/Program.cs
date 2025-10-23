84c5153b8e43fe5b21dbe854b35522ac759e0503
Priyanshumg <ginkalpriyanshu@gmail.com>
2025-10-23 13:16:22 +0530

    [Priyanshu] Solved LeetCode Problem 605 can place flowers

---
+++ 605. Can Place Flowers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _605._Can_Place_Flowers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(CanPlaceFlowers(new int[] { 1, 0, 0, 0, 1 }, 1));
+        Console.WriteLine(CanPlaceFlowers(new int[] { 1, 0, 0, 0, 1 }, 2));
+        Console.WriteLine(CanPlaceFlowers(new int[] { 1, 0, 0, 0, 0, 1 }, 2));
+        Console.WriteLine(CanPlaceFlowers(new int[] { 0, 0, 1, 0, 1 }, 2));
+    }    
+    public static bool CanPlaceFlowers(int[] flowerbed, int n) {
+        for (int i = 0; i < flowerbed.Length; i++)
+        {
+            bool emptyLeft = (i == 0) || (flowerbed[i - 1] == 0);
+            bool emptyRight = (i == flowerbed.Length - 1) ||  (flowerbed[i + 1] == 0);
+            if (flowerbed[i] == 0 && emptyLeft && emptyRight)
+            {
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+        return n <= 0;
+    }
+}

c95e03fcd6bb9b3035f98222afab49a57b035255
Priyanshumg <ginkalpriyanshu@gmail.com>
2025-10-27 12:02:03 +0530

    [Priyanshu] Solved LeetCode Problem 26 using two pointer pattern

---
+++ 26. Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _26._Remove_Duplicates_from_Sorted_Array;
+
+class Program
+{
+    public static int RemoveDuplicates(int[] nums)
+    {
+        int slowPtr = 0;
+
+        for (int fastPtr = 1; fastPtr < nums.Length; fastPtr++)
+        {
+            if (nums[fastPtr] != nums[slowPtr])
+            {
+                slowPtr++;
+                nums[slowPtr] = nums[fastPtr];
+            }        
+        }
+        return slowPtr + 1;
+    }
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine(RemoveDuplicates(new[] { 1, 2, 2, 3, 4, 5, 5 ,6}));
+    }
+}

1001be4de8a278f17a3ad39fe55f2207be26e540
Priyanshumg <ginkalpriyanshu@gmail.com>
2025-10-28 09:42:35 +0530

    [Priyanshu] Solved LeetCode 268. Missing Number with max optimized output

---
+++ 268. Missing Number/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _268._Missing_Number;
+
+class Program
+{
+    public static int MissingNumber(int[] nums)
+    {
+        IEnumerable<int> range = Enumerable.Range(0, nums.Max());
+        foreach (var element in range)
+        {
+            if (!nums.Contains(element)) return element;
+        }
+        return nums.Length;
+    }
+    public static int MissingNumberV2(int[] nums)
+    {
+        int sum = 0;
+        for (int i = 1; i <= nums.Length; i++)
+        {
+            sum += i;
+            sum = sum - nums[i - 1];
+        }
+
+        return sum;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine(MissingNumberV2(new[] { 3, 0, 1 }));
+        Console.WriteLine(MissingNumberV2(new[] { 0, 1 }));
+        Console.WriteLine(MissingNumberV2(new[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 }));
+    }
+}

6fb47dfce3c2e7f1eba9c1f1f6eb9bc6ae8df6b2
Priyanshumg <ginkalpriyanshu@gmail.com>
2025-10-31 11:49:18 +0530

    [Priyanshu] Solved LeetCode 6 Zigzag Conversion

---
+++ 6. Zigzag Conversion/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _6._Zigzag_Conversion;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Convert("PAYPALISHIRING", 3));
+    }
+
+    public static string Convert(string s, int numRows)
+    {
+        if (numRows == 1 || s.Length <= numRows)
+            return s;
+
+        var result = new System.Text.StringBuilder();
+        int cycleLen = 2 * numRows - 2;
+
+        for (int row = 0; row < numRows; row++)
+        {
+            for (int i = row; i < s.Length; i += cycleLen)
+            {
+                result.Append(s[i]);
+
+                int diagonalIndex = i + cycleLen - 2 * row;
+                if (row != 0 && row != numRows - 1 && diagonalIndex < s.Length)
+                    result.Append(s[diagonalIndex]);
+            }   
+        }
+        return result.ToString();
+    }
+}

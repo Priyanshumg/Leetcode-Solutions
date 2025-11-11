3686357b1b9dffdc5560cfca424b8b96a90c8a34
Priyanshumg <ginkalpriyanshu@gmail.com>
2025-11-11 11:25:15 +0530

    [Priyanshu] Solve LeetCode 11 using two pointers solution

---
+++ 11. Container With Most Water/11. Container With Most Water/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _11._Container_With_Most_Water;
+
+class Program
+{
+    public static int MaxArea(int[] arr)
+    {
+        int left = 0, right = arr.Length - 1, max = 0;
+        while (left < right)
+        {
+            int height = arr[left] > arr[right] ? arr[right] : arr[left];
+            max = Math.Max(max, (right - left) * height);
+            _ = arr[left] < arr[right] ? left++ : right--;
+        }
+
+        return max;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Input: [1, 8, 6, 2, 5, 4, 8, 3, 7], Output: {MaxArea(new[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 })}, Expected: 49" );
+        Console.WriteLine($"Input: [1, 1], Output: {MaxArea(new[] { 1, 1 })}, Expected: 1" );
+    }
+}

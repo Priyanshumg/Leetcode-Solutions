3b8b044f493f4fae7684d2ee10c35b16b2f2433f
Priyanshumg <ginkalpriyanshu@gmail.com>
2025-12-12 13:03:21 +0530

    [Priyanshu] Solved Leetcode 27 REmove ELement using inplace value updation

---
+++ 27. Remove Element/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _27._Remove_Element;
+
+class Program
+{
+    public static int RemoveElement(int[] nums, int val)
+    {
+        int k = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine(RemoveElement(new int[] { 3, 2, 2, 3 }, 3));
+        Console.WriteLine(RemoveElement(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2));
+    }
+}

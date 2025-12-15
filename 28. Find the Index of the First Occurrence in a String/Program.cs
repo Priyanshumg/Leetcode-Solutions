9949b24d0e6d5c01f61a770842923725d68caebc
Priyanshumg <ginkalpriyanshu@gmail.com>
2025-12-15 10:56:55 +0530

    [Priyanshu] Solved Leetcode 28 using optimized brute force approach

---
+++ 28. Find the Index of the First Occurrence in a String/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _28._Find_the_Index_of_the_First_Occurrence_in_a_String;
+
+class Program
+{
+    public static int StrStr(string haystack, string needle)
+    {
+        for (int i = 0; i <= haystack.Length - needle.Length; ++i)
+        {
+            if (haystack.Substring(i, needle.Length) == needle)
+                return i;
+        }
+        return -1;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine(StrStr("sadbutsad", "sad"));
+        Console.WriteLine(StrStr("Leetcode", "leeto"));
+        Console.WriteLine(StrStr("a", "a"));
+    }
+}

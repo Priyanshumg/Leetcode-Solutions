57a0366bde09d59ef67724f1705d822f7be54e58
Priyanshumg <ginkalpriyanshu@gmail.com>
2025-12-16 11:39:12 +0530

    [Priyanshu] Solved Leetcode 1 with most optimized solution

---
+++ 1. Two Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+namespace _1._Two_Sum;
+
+class Program
+{
+
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> dict = new();
+        dict[nums[0]] = 0;
+
+        for (int i = 1; i < nums.Length; i++)
+            if (dict.ContainsKey(target - nums[i]))
+                return new int[] { dict[target - nums[i]], i };
+            else
+                dict[nums[i]] = i;
+        
+        return new int[0];
+    }
+
+    public static void print(int[] arr)
+    {
+        Console.Write($"[ {arr[0]}");
+        for (int i = 1; i < arr.Length; i++)
+            if (i == arr.Length - 1)
+                Console.WriteLine($" {arr[i]} ]");
+            else
+                Console.Write($"{arr[i]}, ");
+    }
+    
+    static void Main(string[] args)
+    {
+        print(TwoSum(new int[] {2, 7, 11, 15}, 9));
+        print(TwoSum(new int[] {3, 2, 4}, 6));
+        print(TwoSum(new int[] {3, 3}, 6));
+        
+    }
+}

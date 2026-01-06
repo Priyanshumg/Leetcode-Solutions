fdc239d32a5c9a9e3699e7608b3db0d8d80de0e0
Priyanshumg <ginkalpriyanshu@gmail.com>
2026-01-06 13:49:21 +0530

    [Priyanshu] Added Optimisation to the problem using area based biforgartion on colors

--- 75. Sort Colors/Program.cs
+++ 75. Sort Colors/Program.cs
@@ -2,17 +2,38 @@
 
 class Program
 {
-    public static void SortColors(int[] nums)
+    private static void SortColorsv1(int[] nums)
     {
         for (int i = 0; i <nums.Length; i++)
             for (int j = i + 1; j < nums.Length; j++)
                 if (nums[j] < nums[i])
                     (nums[j], nums[i]) = (nums[i], nums[j]);
     }
-    static void Main(string[] args)
+
+    private static void SortColorsv2(int[] nums)
+    {
+        int low = 0, mid = 0, high = nums.Length - 1;
+        while (mid <= high)
+        {
+            switch (nums[mid])
+            {
+                case 0:
+                    (nums[low], nums[mid]) = (nums[mid], nums[low]);
+                    low++; mid++; break;
+                case 1:
+                    mid++; break;
+                case 2:
+                    (nums[mid], nums[high]) = (nums[high], nums[mid]);
+                    high--; break;
+            }
+        }
+    }
+    
+    
+    private static void Main(string[] args)
     {
         int[] nums = [2, 0, 2, 1, 1, 0];
-        SortColors(nums);
+        SortColorsv2(nums);
         foreach (var element in nums)
             Console.Write(element +  ", ");
     }
--- 75. Sort Colors/obj/Debug/net8.0/75. Sort Colors.AssemblyInfo.cs
+++ 75. Sort Colors/obj/Debug/net8.0/75. Sort Colors.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("75. Sort Colors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6d87348c7094b53c25cce885c2539c2ea6eaa99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5787caafcee3d7fe9de13dff1c5871adb6b51b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("75. Sort Colors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("75. Sort Colors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
